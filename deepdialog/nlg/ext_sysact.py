eb14cdf40921724818a97430b850282cb4d74f83
deepdialog <thebotbot@sina.com>
2019-07-04 21:38:14 +0800

    fix: update

--- deepdialog/nlg/ext_sysact.py
+++ deepdialog/nlg/ext_sysact.py
@@ -1,6 +1,6 @@
 """Automatic load module."""
 
-import os
+# import os
 import importlib
 
 
@@ -8,11 +8,12 @@ EXISTS = {}
 
 
 def load_ext(name):
+    """Load an ext function."""
     global EXISTS
     if name in EXISTS:
         return EXISTS[name]
-    filename = name + '.py'
-    assert os.path.exists(filename)
+    # filename = name + '.py'
+    # assert os.path.exists(filename)
     m = importlib.import_module(name)
     c = getattr(m, name)
     i = c()
